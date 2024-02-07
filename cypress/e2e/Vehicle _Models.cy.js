782bd0a6b8687f5359cf6c070d66fb79c756ba3c
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-02-07 15:26:10 +0100

    vehicle models

---
+++ cypress/e2e/Vehicle _Models.cy.js
@@ -0,0 +1,19 @@
+it("show car details", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('.pick-container__title') // replace with the actual selector for your modal
+        .should('be.visible') // Check if the modal is visible
+        .contains('Vehicle Models');
+
+    cy.get('.pick-description__table__col').should('be.visible').contains('Audi');
+    cy.get('.pick-description__table__col').should('be.visible').contains('AC');
+    
+    /* cy.get('BMW 320 ModernLine'); */
+    cy.get('#btn4').click();
+    cy.get('.pick-description__table__col').should('be.visible').contains('320');
+    cy.get('.pick-description__table__col').should('be.visible').contains('BMW');
+    cy.get('.pick-description__table__col').should('be.visible').contains('AC');
+    cy.get('.pick-description__table__col').should('be.visible').contains('Diesel');
+
+    cy.get('.cta-btn').click();
+    cy.expect('http://localhost:3000/#booking-section');
+});
