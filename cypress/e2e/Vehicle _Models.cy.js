295a16cb40a8144c7ed004399c7b9e9799e1b352
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-02-13 21:21:13 +0100

    adjust comments

--- cypress/e2e/Vehicle _Models.cy.js
+++ cypress/e2e/Vehicle _Models.cy.js
@@ -1,13 +1,12 @@
 it("show car details", () => {
     cy.visit("http://localhost:3000/");
-    cy.get('.pick-container__title') // replace with the actual selector for your modal
+    cy.get('.pick-container__title') 
         .should('be.visible') // Check if the modal is visible
         .contains('Vehicle Models');
 
     cy.get('.pick-description__table__col').should('be.visible').contains('Audi');
     cy.get('.pick-description__table__col').should('be.visible').contains('AC');
     
-    /* cy.get('BMW 320 ModernLine'); */
     cy.get('#btn4').click();
     cy.get('.pick-description__table__col').should('be.visible').contains('320');
     cy.get('.pick-description__table__col').should('be.visible').contains('BMW');
