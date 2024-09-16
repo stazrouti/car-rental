d8ded29c2adaecc1e099429279d48ecf7b753023
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-09-16 13:07:40 +0100

    api routes

---
+++ rental-car-backend/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController; 
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
+
+Route::get('cars', [CarController::class, 'index']);
+Route::post('cars', [CarController::class, 'store']);
+Route::get('cars/{id}', [CarController::class, 'show']);
+Route::put('cars/{id}', [CarController::class, 'update']);
+Route::delete('cars/{id}', [CarController::class, 'destroy']);
+/* Route::middleware('auth:sanctum')->group(function () { */
+/* }); */
